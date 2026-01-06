3af8ea5dfc64d4e2551e821493b8cb4ef0b96ecd
William Kearns <william@heypurpose.com>
2026-01-06 10:59:56 -0800

    Update srl to v0.8.4

--- Formula/srl.rb
+++ Formula/srl.rb
@@ -1,8 +1,8 @@
 class Srl < Formula
   desc "SRL - Spaced Repetition Learning TUI"
   homepage "https://github.com/kearnsw/srl-tui"
-  url "https://github.com/kearnsw/srl-tui/archive/refs/tags/v0.8.3.tar.gz"
-  sha256 "ace07fb9d709fe48e14a0327c6842b05a03045ebc1bea13962c344ae9235b804"
+  url "https://github.com/kearnsw/srl-tui/archive/refs/tags/v0.8.4.tar.gz"
+  sha256 "0ea93ec7aed85c4862c3aeb3095a21001ba23a5de87a84d3c8cb0c4d3c623156"
   license "MIT"
 
   depends_on "rust" => :build

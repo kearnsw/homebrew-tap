5eba852f439898d7fcb926f81726a588719c84a2
William Kearns <william.kearns@proton.me>
2026-01-05 22:11:24 -0800

    Update fl to v0.4.0 - UI overhaul with 10 themes

--- Formula/fl.rb
+++ Formula/fl.rb
@@ -1,8 +1,8 @@
 class Fl < Formula
   desc "Anki-style spaced repetition flashcard TUI"
   homepage "https://github.com/kearnsw/flashcards"
-  url "https://github.com/kearnsw/flashcards/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "853d97ab32558bc7cc1ac56984ceff1c3885bac514ffa37e8262ed0f286ddbc8"
+  url "https://github.com/kearnsw/flashcards/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "03f857a84b5225b9611b03802132bedffdf12cff613fe2a73c5fdd5adadd206a"
   license "MIT"
 
   depends_on "rust" => :build
